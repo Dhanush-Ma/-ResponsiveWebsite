e6268ded90f054949c6617409b3cac4a9852b441
Dhanush-Ma <102428471+Dhanush-Ma@users.noreply.github.com>
2022-07-26 21:40:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 
 const currentTab = window.location.pathname
-const navlinks = document.querySelectorAll("ul a").forEach(link => {
-    if(link.href.includes(`${currentTab}`))
-    {
-        link.classList.add("current-page")
-    }
-})
+
 
 window.addEventListener("load",function(){
     const navlinks = document.querySelectorAll("ul a").forEach(link=> {
